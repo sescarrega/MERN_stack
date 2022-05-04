493262fe32faa5774ffa35aa2271f2a0cb689713
sescarrega <s.jescarrega@gmail.com>
2022-05-04 14:01:45 -0700

    ninja with parent and child class

--- w1d2/ninja/ninja.js
+++ w1d2/ninja/ninja.js
@@ -14,11 +14,34 @@ class Ninja {
         }
 
         showStats(){
-            console.log("${this.name} current stats are: ${this.strength}, ${this.speed}, ${this.health}");
+            console.log(  ` ${this.name}, strength: ${this.strength},speed: ${this.speed}, health: ${this.health}`);
+            // -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
         }
 
         drinkSake(){
             this.health += 10;
         }
 }
+class Sensei extends Ninja{
+    constructor(name){
+        super( name, 200);
+        this.wisdom = 10;
+        // this.health = 200;
+        this.speed= 10;
+        this.strength = 10; 
+        // super( name, 200)
+    }
+    speakWisdom(){
+        this.drinkSake();
+        // console.log("What one programmer can do in one month, two programmers can do in two months");
+        console.log(this.wisdom);
+    }
+}
 
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
