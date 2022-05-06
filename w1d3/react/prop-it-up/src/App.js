bd5dd2f5eb70c68949eb9dcd51924f681ed5f6ea
sescarrega <s.jescarrega@gmail.com>
2022-05-06 14:18:23 -0700

    react app using props and deconstructed vars

---
+++ w1d3/react/prop-it-up/src/App.js
@@ -0,0 +1,13 @@
+import PersonCard from "./componets/PersonCard";
+
+function App(){
+  return(
+    <div>
+      <PersonCard firstName="Jane" lastName="Doe" age="45" hairColor="black" />
+      <PersonCard firstName="Patrick" lastName="Star" age="45" hairColor="bald" />
+      <PersonCard firstName="Charlie" lastName="Brown" age="6" hairColor="bald" />
+    </div>
+  )
+}
+
+export default App;
