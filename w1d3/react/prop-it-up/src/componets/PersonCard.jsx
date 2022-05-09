7d6e020093ee18240c1e7add72511c4837750bdb
sescarrega <s.jescarrega@gmail.com>
2022-05-09 13:37:48 -0700

    prop it up with state changes

--- w1d3/react/prop-it-up/src/componets/PersonCard.jsx
+++ w1d3/react/prop-it-up/src/componets/PersonCard.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
 
 export class PersonCard extends Component {
-    render() {
+    constructor(props){
+        super(props);
+            this.state = {
+                // addYear : age ++       // will this work ?
+                addYear : 55
+            }
+        }
+        addToYear = () =>{
+            this.setState({addYear: this.state.addYear +1})
+        }
+
+
+    render(){
         const{firstName, lastName, age, hairColor} = this.props
         return (
             <div>
                 <h1> {lastName}, {firstName}</h1>
-                <p> age: {age}</p>
                 <p> hair color : {hairColor}</p>
+                {/* <p> age: {age {this.state.addYear}}</p>  */}
+                {/*  if age prop is use will there be conflict  */}
+                <p> age: {this.state.addYear}</p>
+                {/* <button on onClick={({age}) => this.setState({addYear: this.state.addYear  */}
+                {/* +1})}> add to age </button> */}
+
+                <button onClick={this.addToYear}> add a year !</button>
+
             </div>
-    )
-    }
+    );
+}
 }
 
 export default PersonCard
