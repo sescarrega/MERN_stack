fb47f280e117ba23ed5fa2a4c8b2799d6d5e4d02
sescarrega <s.jescarrega@gmail.com>
2022-05-09 19:35:01 -0700

    react app with functional component

--- w1d3/react/prop-it-up/src/App.js
+++ w1d3/react/prop-it-up/src/App.js
@@ -3,9 +3,9 @@ import PersonCard from "./componets/PersonCard";
 function App(){
   return(
     <div>
-      <PersonCard firstName="Jane" lastName="Doe" age="45" hairColor="black" />
-      <PersonCard firstName="Patrick" lastName="Star" age="45" hairColor="bald" />
-      <PersonCard firstName="Charlie" lastName="Brown" age="6" hairColor="bald" />
+      <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="black" />
+      <PersonCard firstName="Patrick" lastName="Star" age={45} hairColor="bald" />
+      <PersonCard firstName="Charlie" lastName="Brown" age={6} hairColor="bald" />
     </div>
   )
 }
--- w1d3/react/prop-it-up/src/componets/PersonCard.jsx
+++ w1d3/react/prop-it-up/src/componets/PersonCard.jsx
@@ -1,35 +1,52 @@
 import React, { Component } from 'react'
+// ----------- BIG INVERSION -------------
+const PersonCard = props => {
+    return (
+        <div>
+            <h1> {props.firstName}  {props.lastName}</h1>
+            <p> age:{props.age}</p>
+            <p>  hair:{ props.hairColor }</p>
 
-export class PersonCard extends Component {
-    constructor(props){
-        super(props);
-            this.state = {
-                // addYear : age ++       // will this work ?
-                addYear : 55
-            }
-        }
-        addToYear = () =>{
-            this.setState({addYear: this.state.addYear +1})
-        }
+        </div>
+    )
 
-
-    render(){
-        const{firstName, lastName, age, hairColor} = this.props
-        return (
-            <div>
-                <h1> {lastName}, {firstName}</h1>
-                <p> hair color : {hairColor}</p>
-                {/* <p> age: {age {this.state.addYear}}</p>  */}
-                {/*  if age prop is use will there be conflict  */}
-                <p> age: {this.state.addYear}</p>
-                {/* <button on onClick={({age}) => this.setState({addYear: this.state.addYear  */}
-                {/* +1})}> add to age </button> */}
-
-                <button onClick={this.addToYear}> add a year !</button>
-
-            </div>
-    );
 }
-}
+
+
+
+// ------------- PROP IT UP AND PUTTING IT TOGETHER --------------
+
+// export class PersonCard extends Component {
+//     constructor(props){
+//         super(props);
+//             this.state = {
+//                 // addYear : age ++       // will this work ?
+//                 addYear : this.props.age
+//             }
+//         }
+//         addToYear = () =>{
+//             this.setState({addYear: this.state.addYear +1}) 
+//         }
+
+
+//     render(){
+//         const{firstName, lastName, age, hairColor} = this.props
+//         return (
+//             <div>
+//                 <h1> {lastName}, {firstName}</h1>
+//                 <p> hair color : {hairColor}</p>
+//                 {/* <p> age: {age {this.state.addYear}}</p>  */}
+//                 {/*  if age prop is use will there be conflict  */}
+//                 <p> age: {this.state.addYear}</p>
+//                 {/* <button on onClick={({age}) => this.setState({addYear: this.state.addYear  */}
+//                 {/* +1})}> add to age </button> */}
+
+//                 <button onClick={this.addToYear}> add a year !</button>
+
+//             </div>
+//     );
+// }
+// }
+
 
 export default PersonCard
